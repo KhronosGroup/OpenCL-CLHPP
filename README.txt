0a05c65e88a70e41a5ac64df04045c65e76d8d3b
Bruce Merry <bmerry@gmail.com@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2013-02-12 14:19:29 +0000

    Add a test suite for cl.hpp. It's very minimal, and currently fails due to bug 9964. Refer to the updated README.txt for instructions on building and running it (there are new external dependencies).
    git-svn-id: https://cvs.khronos.org/svn/repos/OpenCL/branches/CLHPP@20549 e7fa87d3-cd2b-0410-9028-fcbf551c1848

--- README.txt
+++ README.txt
@@ -1,9 +1,9 @@
 cl.hpp base sources.
 
-Three components:
-  input_cl.hpp: 
-    Acts as the master source for the header. 
-	The reason for doing it this way is to generate an appropriate set of functors with varying argument counts without assuming variadic template support in the header.
+Components:
+  input_cl.hpp:
+    Acts as the master source for the header.
+    The reason for doing it this way is to generate an appropriate set of functors with varying argument counts without assuming variadic template support in the header.
 
   gen_cl_hpp.py:
     A generator script written in python to convert input_cl.hpp into cl.hpp, generating the functor expansions as necessary.
@@ -11,7 +11,18 @@ Three components:
   examples:
     A simple example application using the very basic features of the header and generating cl.hpp dynamically through the build system.
 
+  tests:
+    A (very small, incomplete) set of regression tests. Building the tests requires Python, Ruby, Unity and CMock. For the last two I've used Unity 2.0.113 and CMock 2.0.204, but other versions will presumably work too. At the moment there is no way to skip building the tests.
+
   CMake scripts:
     A build system that both generates the example and drives generation of cl.hpp.
 
-After building the example, cl.hpp appears in examples/include. This is the final version that may be moved (with versioning confirmation) into the Khronos/headers tree.
+You need to tell cmake where to find external dependencies, using the variables OPENCL_DIST_DIR, UNITY_DIR and CMOCK_DIR. These can be set either as environment variables, or on the cmake command line using the syntax -D<VAR>=<VALUE>. For the lazy, I use the following commands to build and test (you'll need to adapt your paths):
+
+mkdir build
+cd build
+cmake -DUNITY_DIR=$HOME/src/unity -DCMOCK_DIR=$HOME/src/cmock -DOPENCL_DIST_DIR=/opt/AMD-APP-SDK-v2.7-RC-lnx64/ ..
+make
+tests/test_clhpp
+
+After building, cl.hpp appears in build/include. This is the final version that may be moved (with versioning confirmation) into the Khronos/headers tree.
---
+++ tests/test_clhpp.cpp
@@ -0,0 +1,225 @@
+#include <CL/cl.hpp>
+
+extern "C"
+{
+#include <unity.h>
+#include <cmock.h>
+#include "Mockcl.h"
+#include <string.h>
+
+void setUp()
+{
+}
+
+void tearDown()
+{
+}
+
+void test_CopyContextNonNull()
+{
+    cl_context id1 = (cl_context) 0xdeadbeaf;
+    cl_context id2 = (cl_context) 0xcafebabe;
+
+    clReleaseContext_ExpectAndReturn(id1, CL_SUCCESS);
+    clRetainContext_ExpectAndReturn(id2, CL_SUCCESS);
+
+    cl::Context c1(id1);
+    cl::Context c2(id2);
+    c1 = c2;
+
+    // Prevent the destructor from interfering with the test
+    c1() = NULL;
+    c2() = NULL;
+}
+
+/**
+ * Stub implementation of clGetDeviceInfo that returns a specific device
+ * version.
+ */
+static cl_int clGetDeviceInfo_version(
+    cl_device_id id,
+    cl_device_info param_name,
+    size_t param_value_size,
+    void *param_value,
+    size_t *param_value_size_ret,
+    const char *version)
+{
+    size_t bytes = strlen(version) + 1;
+
+    TEST_ASSERT_NOT_NULL(id);
+    TEST_ASSERT_EQUAL_HEX(CL_DEVICE_VERSION, param_name);
+    TEST_ASSERT(param_value == NULL || param_value_size >= bytes);
+    if (param_value_size_ret != NULL)
+        *param_value_size_ret = bytes;
+    if (param_value != NULL)
+        strcpy((char *) param_value, version);
+    return CL_SUCCESS;
+}
+
+/**
+ * A stub for clGetDeviceInfo that will only support querying
+ * CL_DEVICE_VERSION, and will return version 1.1.
+ */
+static cl_int clGetDeviceInfo_version_1_1(
+    cl_device_id id,
+    cl_device_info param_name,
+    size_t param_value_size,
+    void *param_value,
+    size_t *param_value_size_ret,
+    int num_calls)
+{
+    (void) num_calls;
+    return clGetDeviceInfo_version(
+        id, param_name, param_value_size, param_value,
+        param_value_size_ret, "OpenCL 1.1 Mock");
+}
+
+/**
+ * A stub for clGetDeviceInfo that will only support querying
+ * CL_DEVICE_VERSION, and will return version 1.2.
+ */
+static cl_int clGetDeviceInfo_version_1_2(
+    cl_device_id id,
+    cl_device_info param_name,
+    size_t param_value_size,
+    void *param_value,
+    size_t *param_value_size_ret,
+    int num_calls)
+{
+    (void) num_calls;
+    return clGetDeviceInfo_version(
+        id, param_name, param_value_size, param_value,
+        param_value_size_ret, "OpenCL 1.2 Mock");
+}
+
+void testCopyDeviceNonNull1_1()
+{
+    cl_device_id id1 = (cl_device_id) 0xdeadbeaf;
+    cl_device_id id2 = (cl_device_id) 0xcafebabe;
+
+    clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_version_1_1);
+
+    cl::Device d1(id1);
+    cl::Device d2(id2);
+    d1 = d2;
+}
+
+void testCopyDeviceNonNull1_2()
+{
+    cl_device_id id1 = (cl_device_id) 0xdeadbeaf;
+    cl_device_id id2 = (cl_device_id) 0xcafebabe;
+
+    clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_version_1_2);
+    clReleaseDevice_ExpectAndReturn(id1, CL_SUCCESS);
+    clRetainDevice_ExpectAndReturn(id2, CL_SUCCESS);
+
+    cl::Device d1(id1);
+    cl::Device d2(id2);
+    d1 = d2;
+    // Prevent destructor from interfering with the test
+    d1() = NULL;
+    d2() = NULL;
+}
+
+void testCopyDeviceFromNull1_1()
+{
+    cl_device_id id1 = (cl_device_id) 0xdeadbeaf;
+
+    clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_version_1_1);
+    // No other calls expected
+
+    cl::Device d1(id1);
+    cl::Device d2;
+    d1 = d2;
+}
+
+void testCopyDeviceFromNull1_2()
+{
+    cl_device_id id1 = (cl_device_id) 0xdeadbeaf;
+
+    clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_version_1_2);
+    clReleaseDevice_ExpectAndReturn(id1, CL_SUCCESS);
+
+    cl::Device d1(id1);
+    cl::Device d2;
+    d1 = d2;
+}
+
+void testCopyDeviceToNull1_1()
+{
+    cl_device_id id2 = (cl_device_id) 0xdeadbeef;
+
+    clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_version_1_1);
+    // No other calls expected
+
+    cl::Device d1;
+    cl::Device d2(id2);
+    d1 = d2;
+    // Prevent destructor from interfering with the test
+    d1() = NULL;
+    d2() = NULL;
+}
+
+void testCopyDeviceToNull1_2()
+{
+    cl_device_id id2 = (cl_device_id) 0xdeadbeef;
+
+    clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_version_1_2);
+    clRetainDevice_ExpectAndReturn(id2, CL_SUCCESS);
+
+    cl::Device d1;
+    cl::Device d2(id2);
+    d1 = d2;
+    // Prevent destructor from interfering with the test
+    d1() = NULL;
+    d2() = NULL;
+}
+
+void testCopyDeviceSelf()
+{
+    cl_device_id id = (cl_device_id) 0xdeadbeef;
+
+    // Use 1.2 to check the retain/release calls
+    clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_version_1_2);
+    clReleaseDevice_ExpectAndReturn(id, CL_SUCCESS);
+    clRetainDevice_ExpectAndReturn(id, CL_SUCCESS);
+
+    cl::Device d1(id);
+    cl::Device d2(id);
+    d1 = d2;
+    // Prevent destructor from interfering with the test
+    d1() = NULL;
+    d2() = NULL;
+}
+
+void testAssignDeviceNull()
+{
+    cl::Device d;
+
+    // Any version will do here
+    clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_version_1_1);
+
+    d = (cl_device_id) NULL;
+}
+
+void testDestroyDevice1_1()
+{
+    cl_device_id id = (cl_device_id) 0xdeadbeaf;
+
+    clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_version_1_1);
+    // No other calls expected
+
+    cl::Device d(id);
+}
+
+void testDestroyDevice1_2()
+{
+    cl_device_id id = (cl_device_id) 0xdeadbeaf;
+
+    clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_version_1_2);
+    clReleaseDevice_ExpectAndReturn(id, CL_SUCCESS);
+
+    cl::Device d(id);
+}
+
+} // extern "C"
