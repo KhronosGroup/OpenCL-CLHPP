b73a95cc75b3b8b5a283807fc1bafc803b0195fc
Bruce Merry <bmerry@gmail.com@e7fa87d3-cd2b-0410-9028-fcbf551c1848>
2013-02-14 07:17:21 +0000

    Implement test for getImageInfo<CL_BUFFER>. It currently fails due to an implementation bug (the buffer is not reference-counted because GetInfoHelper<cl::Buffer> has not been specialized.
    git-svn-id: https://cvs.khronos.org/svn/repos/OpenCL/branches/CLHPP@20591 e7fa87d3-cd2b-0410-9028-fcbf551c1848

--- tests/test_clhpp.cpp
+++ tests/test_clhpp.cpp
@@ -25,6 +25,11 @@ static inline cl_device_id make_device_id(int index)
     return (cl_device_id) (size_t) (0xdededede + index);
 }
 
+static inline cl_mem make_mem(int index)
+{
+    return (cl_mem) (size_t) (0x33333333 + index);
+}
+
 /* Pools of pre-allocated wrapped objects for tests. There is no device pool,
  * because there is no way to know whether the test wants the device to be
  * reference countable or not.
@@ -55,13 +60,9 @@ void tearDown()
     }
 }
 
-void test_CopyContextNonNull()
-{
-    clReleaseContext_ExpectAndReturn(make_context(0), CL_SUCCESS);
-    clRetainContext_ExpectAndReturn(make_context(1), CL_SUCCESS);
-
-    contextPool[0] = contextPool[1];
-}
+/****************************************************************************
+ * Stub functions shared by multiple tests
+ ****************************************************************************/
 
 /**
  * Stub implementation of clGetDeviceInfo that just returns the first platform.
@@ -145,6 +146,22 @@ static cl_int clGetPlatformInfo_version_1_2(
         id, param_name, param_value_size, param_value,
         param_value_size_ret, "OpenCL 1.2 Mock");
 }
+
+/****************************************************************************
+ * Tests for cl::Context
+ ****************************************************************************/
+
+void test_CopyContextNonNull()
+{
+    clReleaseContext_ExpectAndReturn(make_context(0), CL_SUCCESS);
+    clRetainContext_ExpectAndReturn(make_context(1), CL_SUCCESS);
+
+    contextPool[0] = contextPool[1];
+}
+
+/****************************************************************************
+ * Tests for cl::Device
+ ****************************************************************************/
 
 void testCopyDeviceNonNull1_1()
 {
@@ -262,6 +279,82 @@ void testDestroyDevice1_2()
     clReleaseDevice_ExpectAndReturn(make_device_id(0), CL_SUCCESS);
 
     cl::Device d(make_device_id(0));
+}
+
+/****************************************************************************
+ * Tests for cl::Image1DBuffer
+ ****************************************************************************/
+
+/**
+ * Stub for querying CL_IMAGE_BUFFER and returning make_mem(1).
+ */
+cl_int clGetImageInfo_testGetImageInfoBuffer(
+    cl_mem image, cl_image_info param_name,
+    size_t param_value_size, void *param_value,
+    size_t *param_value_size_ret,
+    int num_calls)
+{
+    TEST_ASSERT_EQUAL(0, num_calls);
+    TEST_ASSERT_EQUAL_PTR(make_mem(0), image);
+    TEST_ASSERT_EQUAL_HEX(CL_IMAGE_BUFFER, param_name);
+    TEST_ASSERT_EQUAL(sizeof(cl_mem), param_value_size);
+
+    if (param_value != NULL)
+    {
+        *(cl_mem *) param_value = make_mem(1);
+    }
+    if (param_value_size_ret != NULL)
+        *param_value_size_ret = sizeof(cl_mem);
+    return CL_SUCCESS;
+}
+
+void testGetImageInfoBuffer()
+{
+    clGetImageInfo_StubWithCallback(clGetImageInfo_testGetImageInfoBuffer);
+    clRetainMemObject_ExpectAndReturn(make_mem(1), CL_SUCCESS);
+
+    cl::Image1DBuffer image(make_mem(0));
+    cl::Buffer buffer = image.getImageInfo<CL_IMAGE_BUFFER>();
+    TEST_ASSERT_EQUAL_PTR(make_mem(1), buffer());
+
+    // prevent destructor from interfering with the test
+    image() = NULL;
+    buffer() = NULL;
+}
+
+/**
+ * Stub for querying CL_IMAGE_BUFFER and returning NULL.
+ */
+cl_int clGetImageInfo_testGetImageInfoBufferNull(
+    cl_mem image, cl_image_info param_name,
+    size_t param_value_size, void *param_value,
+    size_t *param_value_size_ret,
+    int num_calls)
+{
+    TEST_ASSERT_EQUAL(0, num_calls);
+    TEST_ASSERT_EQUAL_PTR(make_mem(0), image);
+    TEST_ASSERT_EQUAL_HEX(CL_IMAGE_BUFFER, param_name);
+    TEST_ASSERT_EQUAL(sizeof(cl_mem), param_value_size);
+
+    if (param_value != NULL)
+    {
+        *(cl_mem *) param_value = NULL;
+    }
+    if (param_value_size_ret != NULL)
+        *param_value_size_ret = sizeof(cl_mem);
+    return CL_SUCCESS;
+}
+
+void testGetImageInfoBufferNull()
+{
+    clGetImageInfo_StubWithCallback(clGetImageInfo_testGetImageInfoBufferNull);
+
+    cl::Image2D image(make_mem(0));
+    cl::Buffer buffer = image.getImageInfo<CL_IMAGE_BUFFER>();
+    TEST_ASSERT_NULL(buffer());
+
+    // prevent destructor from interfering with the test
+    image() = NULL;
 }
 
 } // extern "C"
