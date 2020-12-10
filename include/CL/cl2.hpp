473e13813fadd78f661e2220eae5bc5f97d75024
Ben Ashbaugh <ben.ashbaugh@intel.com>
2020-12-09 21:42:31 -0800

    switch from warning to pragma message for better portability

--- include/CL/cl2.hpp
+++ include/CL/cl2.hpp
@@ -15,4 +15,4 @@
 //
 
 #include <CL/opencl.hpp>
-#warning cl2.hpp has been renamed to opencl.hpp to make it clear that it supports all versions of OpenCL. Please include opencl.hpp directly.
+#pragma message("cl2.hpp has been renamed to opencl.hpp to make it clear that it supports all versions of OpenCL. Please include opencl.hpp directly.")
