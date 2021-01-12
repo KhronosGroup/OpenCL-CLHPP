37dae4af7a5b66b590c9c4fcc3793f17a64153bb
Ben Ashbaugh <ben.ashbaugh@intel.com>
2021-01-11 22:48:18 -0800

    add test for context creation from device type with non-null properties

--- tests/test_openclhpp.cpp
+++ tests/test_openclhpp.cpp
@@ -601,6 +601,18 @@ void testContextFromType()
     clReleaseContext_ExpectAndReturn(make_context(0), CL_SUCCESS);
 }
 
+void testContextFromTypeNonNullProperties()
+{
+    clCreateContextFromType_StubWithCallback(clCreateContextFromType_testContextFromType);
+
+    const cl_context_properties props[] = {
+        CL_CONTEXT_PLATFORM, (cl_context_properties)make_platform_id(1), 0 };
+    cl::Context context(CL_DEVICE_TYPE_GPU, props);
+    TEST_ASSERT_EQUAL_PTR(make_context(0), context());
+
+    clReleaseContext_ExpectAndReturn(make_context(0), CL_SUCCESS);
+}
+
 static cl_context clCreateContext_testContextNonNullProperties(
     const cl_context_properties* properties,
     cl_uint num_devices,
@@ -616,14 +628,15 @@ static cl_context clCreateContext_testContextNonNullProperties(
     return make_context(0);
 }
 
-void testContextNonNullProperties()
+void testContextWithDeviceNonNullProperties()
 {
     clGetDeviceInfo_StubWithCallback(clGetDeviceInfo_platform);
     clGetPlatformInfo_StubWithCallback(clGetPlatformInfo_version_1_1);
     clCreateContext_StubWithCallback(clCreateContext_testContextNonNullProperties);
     clReleaseContext_ExpectAndReturn(make_context(0), CL_SUCCESS);
 
-    const cl_context_properties props[] = { 0 };
+    const cl_context_properties props[] = {
+        CL_CONTEXT_PLATFORM, (cl_context_properties)make_platform_id(0), 0 };
     cl_device_id device = make_device_id(0);
 
     cl::Context context(cl::Device(device), props);
