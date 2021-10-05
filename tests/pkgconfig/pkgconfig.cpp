6615144185d713ac9a0b7a93061eb9e320deb20b
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2021-10-05 17:51:13 +0200

    Consumption tests

---
+++ tests/pkgconfig/pkgconfig.cpp
@@ -0,0 +1,45 @@
+#include <CL/opencl.hpp>
+
+#include <vector>       // std::vector
+#include <exception>    // std::runtime_error, std::exception
+#include <iostream>     // std::cout
+#include <cstdlib>      // EXIT_FAILURE
+
+int main()
+{
+    try
+    {
+        std::vector<cl::Platform> platforms;
+        cl::Platform::get(&platforms);
+
+        if (platforms.empty()) throw std::runtime_error{ "No OpenCL platforms found." };
+
+        std::cout <<
+            "Found " <<
+            platforms.size() <<
+            " platform" <<
+            (platforms.size() > 1 ? "s.\n" : ".\n") <<
+            std::endl;
+
+        for (const auto& platform : platforms)
+        {
+            std::cout << platform.getInfo<CL_PLATFORM_VENDOR>() << std::endl;
+
+            std::vector<cl::Device> devices;
+            platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+            for (const auto& device : devices)
+                std::cout << "\t" << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+        }
+    }
+    catch (cl::Error& error) // If any OpenCL error occurs
+    {
+        std::cerr << error.what() << "(" << error.err() << ")" << std::endl;
+        std::exit(error.err());
+    }
+    catch (std::exception& error) // If STL/CRT error occurs
+    {
+        std::cerr << error.what() << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
