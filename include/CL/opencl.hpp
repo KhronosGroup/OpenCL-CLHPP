cc6ab20c9c90b529da50512115f83596348cef52
Ben Ashbaugh <ben.ashbaugh@intel.com>
2024-07-31 13:39:32 -0700

    add queries for cl_intel_required_subgroup_size

--- include/CL/opencl.hpp
+++ include/CL/opencl.hpp
@@ -1867,6 +1867,11 @@ CL_HPP_DECLARE_PARAM_TRAITS_(cl_mutable_command_info_khr, CL_MUTABLE_DISPATCH_LO
 CL_HPP_DECLARE_PARAM_TRAITS_(cl_device_info, CL_DEVICE_KERNEL_CLOCK_CAPABILITIES_KHR, cl_device_kernel_clock_capabilities_khr)
 #endif /* cl_khr_kernel_clock */
 
+#if defined(cl_intel_required_subgroup_size)
+CL_HPP_DECLARE_PARAM_TRAITS_(cl_device_info, CL_DEVICE_SUB_GROUP_SIZES_INTEL, cl::vector<size_type>)
+CL_HPP_DECLARE_PARAM_TRAITS_(cl_kernel_work_group_info, CL_KERNEL_SPILL_MEM_SIZE_INTEL, cl_ulong)
+#endif // cl_intel_required_subgroup_size
+
 // Convenience functions
 
 template <typename Func, typename T>
